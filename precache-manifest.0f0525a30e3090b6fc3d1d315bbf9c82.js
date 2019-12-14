9fb4bb5195852e996394344fb76d0369275189f5
Madeleine Prak <madeleineprak@gmail.com>
2019-12-14 10:22:16 -0800

    Updates

---
+++ precache-manifest.0f0525a30e3090b6fc3d1d315bbf9c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28bad7c22c57d5f38d231aa859750df",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "7e9cd9e79b93ce5511f2",
+    "url": "/Memory-Game/static/css/main.a21ddf09.chunk.css"
+  },
+  {
+    "revision": "1ad43d09786347c46248",
+    "url": "/Memory-Game/static/js/2.60ad1cc8.chunk.js"
+  },
+  {
+    "revision": "ffd207a39d3fa30c6aa36be680836db3",
+    "url": "/Memory-Game/static/js/2.60ad1cc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e9cd9e79b93ce5511f2",
+    "url": "/Memory-Game/static/js/main.3bb5cc57.chunk.js"
+  },
+  {
+    "revision": "fa1b34f44f20604a97dc",
+    "url": "/Memory-Game/static/js/runtime-main.9a10409a.js"
+  }
+]);

7b4385b2bba16ee186ae4ecd71df7eb22607df78
Madeleine Prak <madeleineprak@gmail.com>
2020-01-06 00:16:35 -0800

    Reformat input component in input index.js

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,10 +4,21 @@ function Input(props) {
     return (
         <div className="field has-addons">
           <div className="control text-input">
-            <input value={props.search} name="search" className="input is-primary is-medium" onChange={props.handleInputChange} type="text" placeholder="Choose a theme"></input>
+            <input 
+              value={props.search} 
+              name="search" 
+              className="input is-primary is-medium" 
+              onChange={props.handleInputChange} 
+              type="text" 
+              placeholder="Choose a theme">
+            </input>
           </div>
           <div className="control input-button">
-            <button className="button is-primary is-medium" onClick={props.handleUserInput} >Go</button>
+            <button 
+              className="button is-primary is-medium" 
+              onClick={props.handleUserInput} >
+              Go
+            </button>
           </div>
           <div className="score-display">
             <strong>
